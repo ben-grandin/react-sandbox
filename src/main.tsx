acdb33da084769c3705342bcc095bb3aceee4448
Ben Grandin <bengrandin@hotmail.com>
2025-04-15 14:54:38 +0200

    Refactor routing and restructure project files
    Replaced `App` component with `BrowserRouter` for route handling and added new routes. Moved components to a new `pages` directory for better organization. Updated type definitions and minor styling improvements in `SimpleTernary` component.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { SimpleTernary } from "./pages/ternary-or-and/SimpleTernary";
+import TestTernaryOrAndOperator from "./pages/ternary-or-and/TestTernaryOrAndOperator";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="ternary">
+                    <Route path="SimpleTernary" element={<SimpleTernary />} />
+                    <Route path="ternaryOrAnd" element={<TestTernaryOrAndOperator />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
+);
--- src/pages/ternary-or-and/SimpleTernary.tsx
+++ src/pages/ternary-or-and/SimpleTernary.tsx
@@ -1,4 +1,4 @@
-export function Test({ x }) {
+export function Test({ x }: { x: number | null | undefined }) {
     return (
         <div className="p-4 mb-4 bg-white rounded-lg shadow-md border border-gray-200 text-gray-300">
             <h2 className="text-xl font-bold mb-3 text-indigo-700">x: {x + ""}</h2>
@@ -25,7 +25,8 @@ export function SimpleTernary() {
     const unreadMessagesCount = [ 1, 0, null, undefined ];
     return (
         <main className="max-w-4xl mx-auto p-6 bg-gray-50">
-            <h1 className="text-3xl font-bold mb-6 text-center text-indigo-800">Differences between ternary and && operator</h1>
+            <h1 className="text-3xl font-bold mb-6 text-center text-indigo-800">Differences between ternary and &&
+                operator</h1>
 
             <div className="grid gap-4 md:grid-cols-2">
                 {unreadMessagesCount.map((x, i) => (
